7368746e9f00a3821ab442d399c6b5fc8c55e738
Simon Mitchell <simon.mitchell@dvusd.org>
2023-10-17 14:58:15 -0700

    Using Symfony Requirement enums

--- src/Controller/HaddockController.php
+++ src/Controller/HaddockController.php
@@ -6,11 +6,12 @@ use App\Repository\HaddockRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 #[Route('/haddock')]
 class HaddockController extends AbstractController
 {
-    #[Route('/list/{count}', name: 'insult_list', requirements: ['count' => '\d+'], methods: ['GET'])]
+    #[Route('/list/{count}', name: 'insult_list', requirements: ['count' => Requirement::DIGITS], methods: ['GET'])]
     public function list(HaddockRepository $repository, int $count): Response
     {
         $haddock = $repository->findRandomEntries($count);
