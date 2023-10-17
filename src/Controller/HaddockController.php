8f808953b7c1f1d350450cf13a845fdb1bb03c30
Simon Mitchell <simon.mitchell@dvusd.org>
2023-10-17 13:15:28 -0700

    Work on repository

--- src/Controller/HaddockController.php
+++ src/Controller/HaddockController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Haddock;
+use App\Repository\HaddockRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,13 +13,16 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/haddock')]
 class HaddockController extends AbstractController
 {
-    #[Route('/list', name: 'insult_list', methods: ['GET'])]
+    #[Route('/list/{count}', name: 'insult_list', methods: ['GET'])]
     #[Cache(smaxage: 10)]
-    public function list(): Response
+    public function list(EntityManagerInterface $entityManager, int $count): Response
     {
+        $repository = $entityManager->getRepository(Haddock::class);
+        $haddock = $repository->findRandomEntries($count);
+
         $result = [
             'haddock' => [
-                'Ruffian!',
+                $haddock->toArray()
             ],
         ];
 
